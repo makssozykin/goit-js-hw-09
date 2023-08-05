df25427753c4b16fa6f4046a3a9f3f784244ec32
makssozykin <127692421+makssozykin@users.noreply.github.com>
2023-08-05 11:42:38 +0000

    Deploying to gh-pages from @ makssozykin/goit-js-hw-09@12c5e4ef383c3966621d05ffbaf10cd9597b939c 🚀

---
+++ 01-color-switcher.218e18ab.js
@@ -0,0 +1,2 @@
+const t={start:document.querySelector("button[data-start]"),stop:document.querySelector("button[data-stop]"),body:document.querySelector("body")};let e=null;t.start.addEventListener("click",(function(){new Promise((o=>{e=setInterval((()=>{t.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),500)}));t.start.setAttribute("disabled","")})),t.stop.addEventListener("click",(function(){clearInterval(e),t.body.style.backgroundColor=""}));
+//# sourceMappingURL=01-color-switcher.218e18ab.js.map
