12c5e4ef383c3966621d05ffbaf10cd9597b939c
makssozykin <mvsozykin@gmail.com>
2023-08-05 14:42:14 +0300

    first task moduleJS09 is done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
 
+const refs = {
+    start: document.querySelector('button[data-start]'),
+    stop: document.querySelector('button[data-stop]'),
+    body: document.querySelector('body'),
+};
+
+let timerId = null;
+
+refs.start.addEventListener('click', startBtn);
+
+function startBtn() { 
+    const changeColor = new Promise(res => { 
+    timerId = setInterval(() => { 
+       refs.body.style.backgroundColor = getRandomHexColor(); 
+    }, 500)
+    });
+    refs.start.setAttribute("disabled","");
+}
+
+
+
+refs.stop.addEventListener('click', stopBtn);
+
+function stopBtn() { 
+    clearInterval(timerId);
+    refs.body.style.backgroundColor = '';
+}
